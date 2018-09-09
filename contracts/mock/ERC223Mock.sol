913e82e6739f3e8aa68089857938a395090f59f1
Aler Denisov <aler.zampillo@gmail.com>
2018-09-09 19:37:48 +0300

    change mock to new mixins

--- contracts/mock/ERC223Mock.sol
+++ contracts/mock/ERC223Mock.sol
@@ -4,7 +4,7 @@ import "../SmartToken.sol";
 /// @title Custom implementation of ERC223 
 /// @author Aler Denisov <aler.zampillo@gmail.com>
 contract ERC223Mock is SmartToken {
-  constructor() public {
+  constructor() public SmartToken(1000000 ether) {
     balances[msg.sender] = 1000000 ether;
   }
 }
